3b50590310f493ecc0dd3eba802809a28330d2fb
lovrobosnic <lovro.bosnic@superbet.com>
2024-12-26 19:29:12 +0100

    Added all endpoints, readme and exposed .env file

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# url-shortening-service
-URL Shortening Service example
+# URL Shortening Service
+Sample for [URL Shortening Service](https://roadmap.sh/projects/url-shortening-service) project on [roadmap.sh](https://roadmap.sh/)
+
+## Usage
+.env file is intentionally left. You can create your own db user if you wish.
+
+run:
+```bash
+npm i
+```
+and then:
+```bash
+node index.js
+```
+
+When the app is up and running you can use the following endpoints like this:
+Create new shortened url:
+```bash
+    curl --location 'localhost:3000/shorten' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "url": "https://github.com/LoLoCrO/url-shortening-service"
+    }'
+```
+resopnse:
+```json
+{
+    "id": 16,
+    "url": "https://github.com/LoLoCrO/url-shortening-service",
+    "short_code": "BqIyCc",
+    "access_count": 0,
+    "created_at": "2024-12-26T18:17:04.398Z",
+    "updated_at": "2024-12-26T18:17:04.398Z"
+}
+```
+
+Get the long url via `short_code`:
+```bash
+    curl --location 'localhost:3000/shorten/BqIyCc'
+```
+response:
+```json
+{
+    "url": "https://github.com/LoLoCrO/url-shortening-service"
+}
+```
+
+Update an existing url via `short_code`:
+```bash
+    curl --location --request PUT 'localhost:3000/shorten/BqIyCc' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "url": "http://github.com/LoLoCrO/url-shortening-service"
+    }'
+```
+response:
+```json
+{
+    "id": 16,
+    "url": "http://github.com/LoLoCrO/url-shortening-service",
+    "short_code": "BqIyCc",
+    "access_count": 0,
+    "created_at": "2024-12-26T18:17:04.398Z",
+    "updated_at": "2024-12-26T18:20:15.408Z"
+}
+```
+
+Get access count via `shot_code`:
+```bash
+    curl --location --request GET 'localhost:3000/shorten/BqIyCc/stat' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "url": "http://github.com/LoLoCrO/url-shortening-service"
+    }'
+```
+response:
+```json
+{
+    "access_count": 2
+}
+```
+
+Delete url via `short_code`:
+```bash
+    curl --location --request DELETE 'localhost:3000/shorten/BqIyCc'
+```
+response:
+```json
+{
+    "id": 16,
+    "url": "http://github.com/LoLoCrO/url-shortening-service",
+    "short_code": "BqIyCc",
+    "access_count": 2,
+    "created_at": "2024-12-26T18:17:04.398Z",
+    "updated_at": "2024-12-26T18:20:15.408Z"
+}
+```
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import pg from 'pg';
 import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const PORT = process.env.APP_PORT;
 const app = express();
 app.use(express.json());
 
 const pool = new pg.Pool({
-    user: 'url_shortener_user',
-    host: 'localhost',
-    database: 'url_shortener',
-    password: 'password',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 (async () => {
@@ -29,19 +33,101 @@ const pool = new pg.Pool({
 app.post('/shorten', async (req, res) => {
     const { url } = req.body;
     const shortCode = nanoid(6);
-    console.log(url, shortCode);
-    
+
     try {
         const result = await pool.query(
             'INSERT INTO urls (url, short_code) VALUES ($1, $2) RETURNING *',
             [url, shortCode]
         );
+
         res.json(result.rows[0]);
     } catch (err) {
         res.status(400).json({ error: err });
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.get('/shorten/:shortCode', async (req, res) => {
+    const { shortCode } = req.params;
+
+    try {
+        const result = await pool.query(
+            'SELECT * FROM urls WHERE short_code = $1',
+            [shortCode]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'URL not found' });
+        }
+
+        await pool.query(
+            'UPDATE urls SET access_count = access_count + 1, updated_at = CURRENT_TIMESTAMP WHERE short_code = $1 RETURNING *',
+            [shortCode]
+        );
+
+        res.json({ url: result.rows[0].url });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+app.put('/shorten/:shortCode', async (req, res) => {
+    const { shortCode } = req.params;
+    const { url } = req.body;
+
+    try {
+        const result = await pool.query(
+            'UPDATE urls SET url = $1, updated_at = CURRENT_TIMESTAMP WHERE short_code = $2 RETURNING *',
+            [url, shortCode]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'URL not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+app.delete('/shorten/:shortCode', async (req, res) => {
+    const { shortCode } = req.params;
+
+    try {
+        const result = await pool.query(
+            'DELETE FROM urls WHERE short_code = $1 RETURNING *',
+            [shortCode]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'URL not found' });
+        }
+
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+app.get('/shorten/:shortCode/stat', async (req, res) => {
+    const { shortCode } = req.params;
+
+    try {
+        const result = await pool.query(
+            'SELECT * FROM urls WHERE short_code = $1',
+            [shortCode]
+        );
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'URL not found' });
+        }
+
+        res.json({ access_count: result.rows[0].access_count });
+    } catch (error) {
+        res.status(400).json({ error });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
